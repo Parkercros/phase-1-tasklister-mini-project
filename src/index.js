904c9d72091ec59346bfad1ef41c8fb66ab2f7c5
Parker Crosgrove <pdxparker3@gmail.com>
2023-02-06 16:57:42 -0700

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelector('form').removeEventListener('submit', (e) => {
+    e.preventDefault(e.target.create-task-form.value);
+  })
+
 });
+
+function handleToDo (todo) {
+  let p = document. createElement ('p')
+  p. textContent = todo
+  document. querySelector ('#list') .appendchild(p)
+}
